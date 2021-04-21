f384c1a918642d809a1644ea276e9eef45d34cdc
Lindokuhle92 <4115153@myuwc.ac.za>
2021-04-21 13:26:40 +0200

    updated snake homework

--- Lindokuhle_Sibisi_snakes_homework.R
+++ Lindokuhle_Sibisi_snakes_homework.R
@@ -38,9 +38,35 @@ snakes.summary2 <- summarySE(data = snakes, measurevar = "openings", groupvars =
 
 ggplot(data = snakes, aes(x = day, y = openings)) +
   geom_segment(data = snakes.summary2, aes(x = day, xend = day, y = openings - ci, yend = openings + ci, colour = day),
-               size = 3.0, linetype = "solid", show.legend = F) +
-  geom_boxplot(aes(fill = day), alpha = 0.4, show.legend = F) + 
-  geom_jitter(width = 0.07)
+               size = 8.0, linetype = "solid", show.legend = F) +
+  geom_boxplot(aes(fill = day), alpha = 0.1, show.legend = F) + 
+  geom_jitter(width = 0.01)
+
+snakes.summary2 <- summarySE(data = snakes, measurevar = "openings", groupvars = c("day"))
+
+# Create a basic figure
+ggplot(data = snakes, aes(x = openings, y = day)) +
+  geom_plot() +
+  geom_line()
+
+library(ggplot2)
+# Basic line plot with points
+ggplot(data=snakes, aes(x= day, y= openings, group = day)) +
+  geom_line(linetype = "dashed", color = "red")+
+  geom_point(point = "yellow", size = 3)
+
+
+ggplot(data = snakes, aes(x = openings)) +
+  geom_histogram(aes(fill = day), position = "dodge", binwidth = 70) +
+  labs(x = "snakes", y = "count")
+
+
+library(ggplot2)
+# Basic barplot
+p<-ggplot(data=snakes, aes(x=day, y=openings)) +
+  geom_bar(stat="identity", width = 0.5, color = ("blue"), fill = "yellow")
+p
+
 
 snakes.aov <- aov(openings ~ day + snake, data = snakes)
 summary(snakes.aov)
@@ -55,6 +81,8 @@ plot(fitted(snakes.aov), residuals(snakes.aov), col = "blue")
 
 snakes.tukey <- TukeyHSD(snakes.aov, which = "day", conf.level = 0.10)
 plot(snakes.tukey, las = 1, col = "orange")
+
+
 
 
 
